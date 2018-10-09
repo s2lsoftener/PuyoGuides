d2d5990ffbfc2d01fbf701454233e4176a75e0dd
Phong Tran <s2@puyonexus.com>
2018-10-09 19:47:59 -0400

    Began working on Chainsim component

---
+++ src/components/Chainsim.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="chainsim">
+    <h1>Hi</h1>
+    <p>
+      For guide and recipes on how to configure / customize this project,<br>
+      check out the
+      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+    </p>
+    <div ref="game"></div>
+    <br>
+    <button @click="removePuyo">Remove Puyo</button>
+    <button @click="changePosition">Change Puyo Position</button>
+    <button @click="addPuyo">Add Puyo</button>
+    <button @click="changePuyo">Change Puyo</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Chainsim'
+}
+</script>
+
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/views/Simulator.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="simulator">
+    Chain Simulator Editor and Test Page.
+    <chainsim></chainsim>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Chainsim from '@/components/Chainsim.vue'
+
+export default {
+  name: 'simulator',
+  components: {
+    Chainsim
+  }
+}
+</script>
